0ca7e4c92c7b1bba1764f193d1f555c37188425e
Kisara Nishikawa <nishikawa@overwrite.work>
2022-10-12 07:31:01 +0900

    ファイルを追加

---
+++ js/main.js
@@ -0,0 +1,51 @@
+// window.alert("Hello World");
+// let val = "hello";
+// console.log( val );// noFunc();
+// let a = 1;
+// let b = 2;
+// console.log(a + b)
+// let val1 = 789;
+// let val2 = 3;
+// let val3 = 789 % 3;
+// console.log(val1 / val2 );
+
+// let person = {
+//     name: { first:太郎},
+//     age: 18
+// };
+
+// console.log( person.age );
+
+// delete person.age;
+
+// const num = 0;
+// const str = "0";
+// console.log( num === str );
+// const num = null;
+// const str = "null";
+// console.log( num === str );
+// const num = undefined;
+// const str = "undefined";
+// console.log( num === str );
+// console.log( 1 * true)
+// console.log( false * true)
+// console.log( Boolean( 0 ))
+// console.log( Boolean( 1 ))
+// console.log( Boolean( -1 ))
+
+// p80-2
+const TAX_PATH = 1.1;
+let productPrice = 1000;
+// 加算演算子
+console.log( productPrice * TAX_PATH);
+//テンプレートリテラル
+
+3.
+console.log(12 ** 2 % 5);
+// BingInt型
+console.log(12n **2n % 5n);
+
+console.log( typeof null);
+console.log( 100 + true );
+console.log( 1 + Number("Hello"));
+console.log( 1 + Boolean("Hello"));
